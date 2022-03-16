b275eb9213dde46535ebb9721e5f31ff951d0ea3
niiyazobug <niiyaz5555@gmail.com>
2022-03-16 16:40:16 +0600

    Summary React Core Concepts recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<LoadComments></LoadComments>
     </div>
   );
+}
+
+function LoadComments() {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(data => setComments(data))
+
+  }, [])
+
+  return (
+    <div>
+      <h2>Comments: {comments.length}</h2>
+      {
+        comments.map(comment => <Comment email={comment.email} body={comment.body}></Comment>)
+      }
+    </div>
+  )
+}
+
+function Comment(props){
+  return (
+    <div>
+      <h4>email: {props.email}</h4>
+      <p>{props.body}</p>
+    </div>
+  )
 }
 
 export default App;
